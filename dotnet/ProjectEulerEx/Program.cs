4640045419fcb224a87aed721d2b7dba2bd8a4e1
keb <kebenit@outlook.com>
2021-09-04 18:31:50 -0500

    Solved problem 1 and 2

---
+++ dotnet/ProjectEulerEx/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ProjectEulerLib;
+
+using static System.Console;
+
+
+
+namespace ProjectEulerEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Problem001 problem001 = new Problem001();
+            problem001.SolveProblem();
+            Problem002 problem002 = new Problem002();
+            problem002.SolveProblem();
+        }
+    }
+}
---
+++ dotnet/ProjectEulerLib/Problem001.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using static System.Console;
+
+namespace ProjectEulerLib
+{
+    public class Problem001
+    {
+        /*
+            If we list all the natural number below 10 that are multiples of 3 or 5, we get 3, 5, 6, and 9. The sum of these multiples is 23
+            Find the sum of all multiples of 3 or 5 below 1000.
+        */
+
+        int finalSum;
+        public void SolveProblem(int length = 1000)
+        {
+            Write("Problem 001 solution: ");
+            for (int i = 0; i < length; i++)
+            {
+                //Write(i + " ");
+                if ((i % 3) == 0)
+                {
+                    finalSum += i;
+                }
+                else if ((i % 5) == 0)
+                {
+                    finalSum += i;
+                }
+            }
+            WriteLine($"The sum is {finalSum}");
+        }
+    }
+}

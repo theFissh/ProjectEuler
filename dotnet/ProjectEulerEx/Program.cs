2c6a3994fbc3b3acd4b0b5107c3459e584288e65
Fissh <kebenit@outlook.com>
2021-09-29 23:51:58 -0700

    Solved problem004

--- dotnet/ProjectEulerEx/Program.cs
+++ dotnet/ProjectEulerEx/Program.cs
@@ -13,7 +13,9 @@ namespace ProjectEulerEx
         {
             // Problem001 problem001 = new Problem001(); problem001.SolveProblem();
             // Problem002 problem002 = new Problem002(); problem002.SolveProblem();
-            Problem003 problem003 = new Problem003(); problem003.SolveProblem();
+            // Problem003 problem003 = new Problem003(); problem003.SolveProblem();
+            Problem004 problem004 = new Problem004(); problem004.SolveProblem();
+            WriteLine("Done");
         }
     }
 }

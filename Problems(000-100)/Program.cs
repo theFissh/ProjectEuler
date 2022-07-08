db4de4ca870678c90a30a0f6015dbda7fc898268
Fissh <ferchovelarr@outlook.com>
2022-07-07 19:23:04 -0500

    Solved problem 001
    Added Jupyter notebook

---
+++ Problems(000-100)/Program.cs
@@ -0,0 +1,7 @@
+﻿// Problem 001
+// * Solved!
+List<int> Multiples = new();
+for (int i = 0; i < 1000; i++)
+    if ((i % 3 == 0) | (i % 5 == 0))
+        Multiples.Add(i);
+Console.WriteLine(Multiples.Sum());
